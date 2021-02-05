cd7c67f5fff0aac2a04dd5b11bb6d75ef9d0f99b
Bence Agocs <bence@harom.studio>
2021-02-05 14:51:21 +0100

    hamburger icon updated

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,31 +1,11 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var accWrap = this.parentElement;
-    accWrap.classList.toggle('active');
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-    } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-    });
-}
-const hamburger = document.querySelector(".navbar-toggler"),
+const hamburger = document.querySelector(".hamburger"),
       navbar = document.querySelector('.navbar-nav'),
-      content = document.querySelector('.page-content');
+      content = document.querySelector('.page-content'),
+      body = document.querySelector('BODY');
 
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
     navbar.classList.toggle("slide-right");
     content.classList.toggle("slide-left");
-});
-const headerTechData = document.querySelector(".tech-details-popup"),
-      buttonTechData = document.querySelector('.btn-tech-details');
-
-buttonTechData.addEventListener("click", function() {
-    headerTechData.classList.toggle("hide");
+    body.classList.toggle("lock-scroll");
 });
--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,9 +1,11 @@
-const hamburger = document.querySelector(".navbar-toggler"),
+const hamburger = document.querySelector(".hamburger"),
       navbar = document.querySelector('.navbar-nav'),
-      content = document.querySelector('.page-content');
+      content = document.querySelector('.page-content'),
+      body = document.querySelector('BODY');
 
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
     navbar.classList.toggle("slide-right");
     content.classList.toggle("slide-left");
+    body.classList.toggle("lock-scroll");
 });
