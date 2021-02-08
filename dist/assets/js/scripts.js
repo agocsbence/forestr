159c4eec4f1855d6052b9430677dbaa060f04b7b
Bence Agocs <bence@harom.studio>
2021-02-08 11:21:48 +0100

    pages updated

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector(".hamburger"),
       navbar = document.querySelector('.navbar-nav'),
       content = document.querySelector('.page-content'),
-      body = document.querySelector('BODY');
+      body = document.querySelector('BODY'),
+      footer = document.querySelector('footer .social');
+
+if(body.classList.contains("bg--green")) {
+    hamburger.classList.add("content--white");
+    footer.classList.add("content--white");
+}
 
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
