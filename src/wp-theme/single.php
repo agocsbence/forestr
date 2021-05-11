4414715bfb1bf4c29887e3e5471b2b68f69780c0
Bence Agocs <bence@harom.studio>
2021-05-12 00:25:57 +0200

    slider updated

--- src/wp-theme/single.php
+++ src/wp-theme/single.php
@@ -22,7 +22,7 @@
 
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script>
-      var swiper = new Swiper(".mySwiper", {
+      var swiper = new Swiper("#mySwiper", {
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
