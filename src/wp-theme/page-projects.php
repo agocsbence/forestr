5d278e6ae06513fa1a6ac5f5367076fe946d6ab6
Bence Agocs <bence@harom.studio>
2021-05-13 09:35:57 +0200

    projects page updated

--- src/wp-theme/page-projects.php
+++ src/wp-theme/page-projects.php
@@ -24,29 +24,6 @@ get_header();
 			endwhile; wp_reset_query();
 		?>
 
-        <div class="news-post">
-            <div class="grid grid-2">
-                <div class="news-image" style="background-image: url('./assets/img/projects-1.jpg');"></div>
-                <div class="news-text">
-                    <h3>The vision</h3>
-                    <p>Our major role in the fight against climate change is to interconnect motivated & proactive individuals and organizations to plant trees, find fallow areas and act against deforestation.</p>
-                    <p>We need to rethink our role in Earth’s ecosystem.<br>
-                        We wish to reform our thinking about trees and nature, we wish to introduce cutting edge technologies that can substitute the tree industry in general. <br>
-                        We wish to open the eyes of company leaders and stakeholders about the actual unsustainable methods using the tree industry…</p>
-                </div>
-            </div>
-        </div>
-        
-        <div class="news-post">
-            <div class="grid grid-2">
-                <div class="news-image" style="background-image: url('./assets/img/projects-2.jpg');"></div>
-                <div class="news-text">
-                    <h3>Forestr Tree Planting Guide</h3>
-                    <p>Ever wondered about how to plant a tree? Here is a quickie guide where to start.</p>
-                </div>
-            </div>
-        </div>
-
     </div>
 </section>
 
